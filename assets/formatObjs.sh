311bb74e27604bf917bedda4616fde3717d91c06
graefjk <st161399@stud.uni-stuttgart.de>
2025-01-16 02:03:17 +0100

    added script to format folder of .obj files

---
+++ assets/formatObjs.sh
@@ -0,0 +1,11 @@
+if [ "$1" = "--help" ]; then
+echo "./formatObjs.sh [folder containing .obj files] [scale]"
+else
+mkdir $1/copy
+for file in $(ls -p $1 | grep -v / | grep "\.obj$")
+do
+    cat $1/$file | awk -v scale="$2" '{if ($1 == "v") {print $1 " " $2*scale " " $3*scale " " $4*scale} else if ($1 == "f") {print $0}}' > "$1/copy/$file"
+done
+mv $1/copy/*.obj $1/
+rm -rf $1/copy
+fi

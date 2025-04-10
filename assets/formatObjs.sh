02258129f11e8ffefb19949eaba55e5eb37b54b6
st161399 <st161399@stud.uni-stuttgart.de>
2025-04-10 03:04:09 +0200

    List all vertices before all the Polygon faces

--- assets/formatObjs.sh
+++ assets/formatObjs.sh
@@ -1,11 +1,16 @@
 if [ "$1" = "--help" ]; then
-echo "./formatObjs.sh [folder containing .obj files] [scale]"
+	echo "./formatObjs.sh [folder containing .obj files] [scale]"
 else
-mkdir $1/copy
-for file in $(ls -p $1 | grep -v / | grep "\.obj$")
-do
-    cat $1/$file | awk -v scale="$2" '{if ($1 == "v") {print $1 " " $2*scale " " $3*scale " " $4*scale} else if ($1 == "f") {print $0}}' > "$1/copy/$file"
-done
-mv $1/copy/*.obj $1/
-rm -rf $1/copy
+	scale=$2
+	if [ -z "$scale" ]; then
+		scale=1;
+	fi
+
+	mkdir $1/copy
+	for file in $(ls -p $1 | grep -v / | grep "\.obj$")
+	do
+	    cat $1/$file | awk -v scale="$scale" '{if ($1 == "v") {print $1 " " $2*scale " " $3*scale " " $4*scale} else if ($1 == "f") {print $0}}' | sort -srk 1,1 > "$1/copy/$file"
+	done
+	mv $1/copy/*.obj $1/
+	rm -rf $1/copy
 fi
